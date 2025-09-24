c8ba54b3063b1309606b50ae7874768df77f5a88
Mohammed Farhaan <mdfarhaan1044@gmail.com>
2025-09-24 19:04:25 +0530

    Counter App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Counter from './components/Counter'
 
 // This is the App component. The componenet should be always start with uppercase
 function App() {
@@ -6,7 +7,7 @@ function App() {
     return (
       // In the return statement we can write html inside javascript. And we can only return one element so either we can use div or fragment , Fragment is empty element <> </> we can also use div but it will add an extra element to the DOM so its better to use React Fragment
      <>
-     <h1>React Basic</h1>
+    <Counter/>
      </>
      
     )
---
+++ src/components/Counter.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+function Counter() {
+
+    // let count = 0; This is not a state variable so we cant update it we need to use useState hook 
+
+    const [count, setCount] = useState(0);
+    // This hooks is used to change the ui when the state changes and it changes every time when the state changes and everywhere it is used
+
+    // Simple function to add count
+    const addCount = () => {
+        // Check if count is 20
+        if (count === 20) {
+            alert("Cant go above 20");
+            return;
+        }
+        // Update the state
+        setCount(count + 1);
+    }
+
+    // Simple function to remove count
+    const removeCount = () => {
+        // Check if count is 0
+        if (count === 0) {
+            alert("Cant go below 0");
+            return;
+        }
+           // Update the state
+        setCount(count - 1);
+    }
+
+    // Return statement
+    return (
+        <>
+            <h1>Counter App</h1>
+            <h2>Count: {count}</h2>
+            {/* Buttons to add and reset count */}
+            <button onClick={addCount}>Add +1</button>
+            <button onClick={removeCount}>Remove -1</button>
+        </>
+    )
+}
+
+// Export the component
+export default Counter
