db50e8d64cbc4d17c39b6d8112a65487ad6f97c8
Mohammed Farhaan <mdfarhaan1044@gmail.com>
2025-09-27 12:10:01 +0530

    BG Color change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
 import Counter from './components/Counter'
+import BgChange from './components/BgChange'
 
 // This is the App component. The componenet should be always start with uppercase
 function App() {
   // This is the return statement
-    return (
-      // In the return statement we can write html inside javascript. And we can only return one element so either we can use div or fragment , Fragment is empty element <> </> we can also use div but it will add an extra element to the DOM so its better to use React Fragment
-     <>
-    <Counter/>
-     </>
-     
-    )
-  }
-  
-  
-  // This is the export statement after export we can use this component in other files
-  export default App
+
+  return (
+
+    // In the return statement we can write html inside javascript. And we can only return one element so either we can use div or fragment , Fragment is empty element <> </> we can also use div but it will add an extra element to the DOM so its better to use React Fragment
+    <div>
+      {/* 1.Counter App <Counter/> */}
+      {/* 2. Background Change App <BgChange /> */}
+    </div>
+
+  )
+}
+
+
+// This is the export statement after export we can use this component in other files
+export default App
---
+++ src/components/BgChange.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+// This is the BgChange component. The componenet should be always start with uppercase
+function BgChange() {
+    // useState hooks to store the background color
+    const [bgcolor, setBgcolor] = useState('black')
+    // function to change the background color
+    const changeBg = (color) => {
+        setBgcolor(color)
+    }
+
+    return (
+        <div className="flex justify-center gap-2 w-full items-center h-screen" style={{ backgroundColor: bgcolor }}>
+            {/* buttons to change the background color */}
+            <button className="px-2 py-1 bg-red-500 text-black" onClick={() => changeBg('red')}>Red</button>
+            <button className="px-2 py-1 bg-green-500 text-black" onClick={() => changeBg('green')}>Green</button>
+            <button className="px-2 py-1 bg-blue-500 text-black" onClick={() => changeBg('blue')}>Blue</button>
+            <button className="px-2 py-1 bg-yellow-500 text-black" onClick={() => changeBg('yellow')}>Yellow</button>
+            <button className="px-2 py-1 bg-purple-500 text-black" onClick={() => changeBg('purple')}>Purple</button>
+            <button className="px-2 py-1 bg-orange-500 text-black" onClick={() => changeBg('orange')}>Orange</button>
+            <button className="px-2 py-1 bg-pink-500 text-black" onClick={() => changeBg('pink')}>Pink</button>
+            <button className="px-2 py-1 bg-black text-white" onClick={() => changeBg('black')}>Black</button>
+            <button className="px-2 py-1 bg-white text-black" onClick={() => changeBg('white')}>White</button>
+        </div>
+    )
+}
+
+export default BgChange
